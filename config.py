f8162082484e852f7489eec5c3eed0f25e3ee4ed
ZnCuHHH <65380493+ZnCuHHH@users.noreply.github.com>
2021-04-20 21:22:20 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 conf = {
-    "WORK_PATH": "./work",
+    "WORK_PATH": "/content/zncu/work",
     "CUDA_VISIBLE_DEVICES": "0",# gpu编号  原程序：0,1,2,3
     "data": {
-        'dataset_path': "D:\\Gait\\",
+        'dataset_path': "/content/set/Gait",
         'resolution': '64',
         'dataset': 'CASIA-B',
         # In CASIA-B, data of subject #5 is incomplete.
         # Thus, we ignore it in training.
         # For more detail, please refer to
         # function: utils.data_loader.load_data
-        'pid_num': 124,
+        'pid_num': 20,
         'pid_shuffle': False,
     },
     "model": {
@@ -18,7 +18,7 @@ conf = {
         'hard_or_full_trip': 'full',
         'batch_size': (2, 8),
         'restore_iter': 0,
-        'total_iter': 80000,
+        'total_iter': 30000,
         'margin': 0.2,
         'num_workers': 0,
         'frame_num': 30,
